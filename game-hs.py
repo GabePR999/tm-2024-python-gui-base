3e21dcc2677bf056df77ec8bbc123effe5b5fb03
GabePR999 <gabriel.rodriguez@timothyministry.org>
2025-03-04 13:55:12 +0000

    updates

--- game-hs.py
+++ game-hs.py
@@ -1,25 +1,28 @@
 import pyglet
 from pyglet.shapes import Rectangle, Circle
 from pyglet.window import key
+from pyglet.text import Label
+
 from random import randint
 
 #windows config
 window_width = 800
 window_height = 600
-window = pyglet.window.Window(width=window_width, height=window_height, caption="Foo Bar")
+window = pyglet.window.Window(width=window_width, height=window_height, caption="My Game")
 
 #color constants
 GRAY = (169,169,169)
 GREEN = (34,139,34)
 WHITE = (255,255,255)
 RED = (255,0,0,)
+
 #player config
 player_radius = 40
 player_x = window_width / 2
 player_y = 100
 player_speed = 150
 
-#This handles keyboard input
+# This handles keyboard input
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
@@ -30,10 +33,18 @@ obstacle_size = 60
 obstacle_speed = 200 
 obstacles = []
 
-def add_obstacle():
-    x = randint(path_x, path_x + path_width - obstacle_size)
-    obstacle = Rectangle (200, window_height-obstacle_size, obstacle_size, obstacle_size, RED)
-    obstacles.apped (obstacle)
+def add_obstacle(dt):
+    x = randint(int(path_x), int(path_x + path_width - obstacle_size))
+    obstacle = Rectangle(x, window_height-obstacle_size,obstacle_size,obstacle_size,RED)
+    obstacles.append(obstacle)
+
+def check_collision():
+    for obstacle in obstacles:
+       if (player.x - player_radius < obstacle.x + obstacle.width and
+        player.x + player_radius > obstacle.x and
+        player.y - player_radius < obstacle.y + obstacle.height
+        player.y + player_radius > obstacle.y):
+            return obstacle
 
 path_width = 400
 path_x = (window_width - path_width)/2
@@ -41,9 +52,24 @@ path_x = (window_width - path_width)/2
 background = Rectangle(0,0,window_width, window_height, GREEN )
 
 path = Rectangle(path_x,0,path_width,window_height,GRAY)
-dir = 'right'
-def update(dt):
 
+points_label = Label(
+    'Points: 0',
+    font_name='Arial',
+    font_size=18,x=10,y=window_height-30
+)
+health = 100
+health_label = Label(f"Health: {health}",
+                    font_name="Arial",
+                    font_size=18, x=10,y=window_height-60)
+
+points = 0 
+def update_points(dt):
+    global points
+    health -= 10
+    points_label.text = f'Points: {points}'
+
+def update(dt):
     if keys[key.LEFT]:
         player.x -= player_speed * dt
     if keys[key.RIGHT]:
@@ -55,30 +81,16 @@ def update(dt):
     if player.x < path_x+player_radius:
         player.x = path_x + player_radius
 
-for obstacle in obstacles [:]:
-    obstacle.y -= obstacle_speed * dt
-    
-    if obstacle.y + obstacle_size <0:
-        obstacle.delete()
-        obstacles.remove(obstacle)
+    for obstacle in obstacles[:]:
+        obstacle.y -= obstacle_speed * dt
 
+        if obstacle.y + obstacle_size < 0:
+            obstacle.delete()
+            obstacles.remove(obstacle) 
 
-obstacle_size = 60
-obstacle_speed = 200 
-
-obstacle = Rectangle (200, window_height-obstacle_size, obstacle_size, obstacle_size, RED)
-
-
-#This moves the player left and right
-    # if dir == 'right':
-    #     player.x = player.x + player_speed * dt
-    # elif dir == 'left':
-    #     player.x = player.x - player_speed * dt
-
-    # if player.x > path_width+path_x-player_radius and dir == 'right':
-    #     dir = 'left'
-    # elif player.x < path_x+player_radius and dir =='left':
-    #     dir = 'right'
+    obstacle_hit = check_collision()
+    if obstacle_hit:
+    print(obstacle_hit)
 
 @window.event
 def on_draw():
@@ -88,6 +100,10 @@ def on_draw():
     background.draw()
     for obstacle in obstacles[:]:
         obstacle.draw()
+    points_label.draw()
+    health_label.draw()
 
+pyglet.clock.schedule_interval(update_points,1)
+pyglet.clock.schedule_interval(add_obstacle, 2)
 pyglet.clock.schedule_interval(update, 1/60.0)
 pyglet.app.run()
